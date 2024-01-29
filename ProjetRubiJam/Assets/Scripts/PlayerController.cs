341c30dec03087728847dad412f0d623676059bc
PHILIPPON <m.philippon@rubika-edu.com>
2024-01-29 16:29:13 +0100

    Fixed Axis Direction

--- ProjetRubiJam/Assets/Scripts/PlayerController.cs
+++ ProjetRubiJam/Assets/Scripts/PlayerController.cs
@@ -3,49 +3,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private MonkInputAction _monkInputAction;
     private int _playerIndex;
 
     [SerializeField] private Rigidbody rbMonk;
-    [SerializeField] private PlayerInput inputMonk;
     [SerializeField] private float accel = 100;
-    
-    // private void Awake()
-    // {
-    //     _monkInputAction = new MonkInputAction();
-    //
-    //     _playerIndex = inputMonk.playerIndex;
-    // }
-    //
-    // private void OnEnable()
-    // {
-    //     switch (_playerIndex)
-    //     {
-    //         case 0:
-    //             _monkInputAction.Player1.Enable();
-    //             _monkInputAction.Player1.Move.performed += context => Move(context.ReadValue<Vector2>());
-    //             break;
-    //         case 1:
-    //             _monkInputAction.Player2.Enable();
-    //             _monkInputAction.Player2.Move.performed += context => Move(context.ReadValue<Vector2>());
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    // }
-    //
-    // private void OnDisable()
-    // {
-    //     _monkInputAction.Disable();
-    // }
-    //
-    // private void Move(Vector2 direction)
-    // {
-    //     rbMonk.AddForce(direction*accel,ForceMode.Impulse);
-    // }
+    [SerializeField] private float maxSpeed = 8;
     
     public void Move(InputAction.CallbackContext ctx)
     {
-        rbMonk.AddForce(ctx.ReadValue<Vector2>()*accel,ForceMode.Impulse);
+        var direction = ctx.ReadValue<Vector2>();
+        rbMonk.AddForce(new Vector3(direction.x,0,direction.y)*accel,ForceMode.Impulse);
+        if (rbMonk.velocity.magnitude > maxSpeed) rbMonk.velocity = rbMonk.velocity.normalized * maxSpeed;
     }
 }
