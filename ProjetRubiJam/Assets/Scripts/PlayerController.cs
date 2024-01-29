9b0a924e4c36814e7434084c05953ce0fdc443e5
PHILIPPON <m.philippon@rubika-edu.com>
2024-01-29 17:30:32 +0100

    Press & Release Action

--- ProjetRubiJam/Assets/Scripts/PlayerController.cs
+++ ProjetRubiJam/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private int _playerIndex;
+    private InteractableObj _currentObj;
+    private bool _interacting;
 
     [SerializeField] private Rigidbody rbMonk;
     [SerializeField] private float accel = 100;
@@ -14,5 +16,32 @@ public class PlayerController : MonoBehaviour
         var direction = ctx.ReadValue<Vector2>();
         rbMonk.AddForce(new Vector3(direction.x,0,direction.y)*accel,ForceMode.Impulse);
         if (rbMonk.velocity.magnitude > maxSpeed) rbMonk.velocity = rbMonk.velocity.normalized * maxSpeed;
+    }
+    
+    public void Interact(InputAction.CallbackContext ctx)
+    {
+        Debug.Log(ctx.performed);
+        if (ctx.performed) Press();
+        else Release();
+    }
+
+    private void Press()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.forward.normalized*3,Color.blue);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 3))
+        {
+            if (hit.transform.gameObject.layer != 6) return; //layer 6 = interaction
+            _currentObj = hit.transform.GetComponent<InteractableObj>();
+            _currentObj.PressAction();
+            _interacting = true;
+        }
+    }
+    
+    private void Release()
+    {
+        if (!_interacting) return;
+        _currentObj.ReleaseAction();
+        _interacting = false;
     }
 }
