b4c8d4979bffc340960ebbc30868256f379f3f8a
SUN <f.sun@rubika-edu.com>
2024-01-29 14:16:28 +0100

    fix: monkmanager

---
+++ ProjetRubiJam/Assets/Scripts/MonkManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkManager : MonoBehaviour
+{
+    [SerializeField] private int jaugeMoney = 50;
+    [SerializeField] private int jaugePeopleLove = 50;
+    [SerializeField] private int jaugeGodFaith = 50;
+    
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    void GameOver()
+    {
+        
+    }
+    
+}
