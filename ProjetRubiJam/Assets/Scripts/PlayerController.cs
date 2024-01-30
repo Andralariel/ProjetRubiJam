3e52d6e78d96df221fc6e77c6345e6c4e09a32b5
PHILIPPON <m.philippon@rubika-edu.com>
2024-01-30 11:25:56 +0100

    Fixed Wiggle and Upgraded Interaction with BoxCast

--- ProjetRubiJam/Assets/Scripts/PlayerController.cs
+++ ProjetRubiJam/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,22 +7,17 @@ public class PlayerController : MonoBehaviour
     private int _playerIndex;
     private InteractableObj _currentObj;
     private bool _interacting;
+    private Vector3 _currentDirection;
+    
     [SerializeField] private LayerMask layerMask;
-
     [SerializeField] private Rigidbody rbMonk;
     [SerializeField] private float accel = 100;
     [SerializeField] private float maxSpeed = 8;
     
     public void Move(InputAction.CallbackContext ctx)
     {
-        if (!ctx.performed) return;
-        
-        Vector3 direction = ctx.ReadValue<Vector2>();
-        direction = new Vector3(direction.x, 0, direction.y);
-        rbMonk.AddForce(direction*accel,ForceMode.Impulse);
-        if (rbMonk.velocity.magnitude > maxSpeed) rbMonk.velocity = rbMonk.velocity.normalized * maxSpeed;
-        
-        transform.rotation = Quaternion.LookRotation(direction);
+         _currentDirection = ctx.ReadValue<Vector2>();
+         _currentDirection = new Vector3(_currentDirection.x, 0, _currentDirection.y);
     }
     
     public void Interact(InputAction.CallbackContext ctx)
@@ -36,7 +32,7 @@ public class PlayerController : MonoBehaviour
         RaycastHit hit;
         Debug.DrawRay(transform.position, transform.forward.normalized*3,Color.blue);
         
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 3,layerMask)) //layer interaction
+        if(Physics.BoxCast(transform.position,new Vector3(0,1,0.5f),transform.forward,out hit,Quaternion.LookRotation(transform.forward),3,layerMask))
         {
             _currentObj = hit.transform.GetComponent<InteractableObj>();
             _currentObj.PressAction(this);
@@ -49,5 +45,13 @@ public class PlayerController : MonoBehaviour
         if (!_interacting) return;
         _currentObj.ReleaseAction();
         _interacting = false;
+    }
+
+    private void FixedUpdate()
+    {
+        rbMonk.AddForce(_currentDirection*accel,ForceMode.Impulse);
+        if (rbMonk.velocity.magnitude > maxSpeed) rbMonk.velocity = rbMonk.velocity.normalized * maxSpeed;
+        
+        if(_currentDirection!=Vector3.zero) transform.rotation = Quaternion.LookRotation(_currentDirection);
     }
 }
