10d15e1cdf3f5b15ef0b7c9188198cdba57acfe3
PHILIPPON <m.philippon@rubika-edu.com>
2024-01-31 17:08:22 +0100

    InsurrectionFix

--- ProjetRubiJam/Assets/Scripts/Events/MoneyEvent.cs
+++ ProjetRubiJam/Assets/Scripts/Events/MoneyEvent.cs
@@ -6,12 +6,12 @@ namespace Events
     {
         public override void StartEvent()
         {
-            Debug.Log("Start of MoneyEvent");
+            MonkManager.instance.famine = true;
         }
 
         public override void EndEvent()
         {
-            Debug.Log("End of MoneyEvent");
+            MonkManager.instance.famine = false;
         }
     }
 }
